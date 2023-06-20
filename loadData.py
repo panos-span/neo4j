14facb72cc508ec37e6044756c78c3be3547f85a
Panagiotis Spanakis <t8200158@aueb.gr>
2023-06-21 00:24:43 +0300

    Optimize loadData.py (and add timestamp field to action) #1

--- loadData.py
+++ loadData.py
@@ -1,7 +1,6 @@
 import pandas as pd
 from neo4j import GraphDatabase
 
-# Replace 'file.tsv' with the paths to your TSV files
 actions = pd.read_csv('mooc_actions.tsv', delimiter='\t', names=['ACTIONID', 'USERID', 'TARGETID', 'TIMESTAMP'],
                       skiprows=1)
 action_features = pd.read_csv('mooc_action_features.tsv', delimiter='\t',
@@ -19,21 +18,20 @@ driver = GraphDatabase.driver(uri, auth=(username, password))
 
 with driver.session() as session:
     # Create user nodes
-    for user_id in actions['USERID'].unique():
-        session.run("CREATE (:User {id: $id})", id=user_id)
+    users = list(actions['USERID'].unique())
+    session.run("UNWIND $ids AS id CREATE (:User {id: id})", ids=users)
 
     # Create target nodes
-    for target_id in actions['TARGETID'].unique():
-        session.run("CREATE (:Target {id: $id})", id=target_id)
+    targets = list(actions['TARGETID'].unique())
+    session.run("UNWIND $ids AS id CREATE (:Target {id: id})", ids=targets)
 
     # Create action relationships
-    for index, row in actions.iterrows():
-        session.run("""
-            MATCH (user:User {id: $user_id})
-            MATCH (target:Target {id: $target_id})
-            CREATE (user)-[:ACTION {id: $action_id, feature0: $feature0, feature1: $feature1,
-            feature2: $feature2, feature4: $feature4, label: $label}]->(target)
-            """, user_id=row['USERID'], target_id=row['TARGETID'], action_id=row['ACTIONID'], feature0=row['FEATURE0'],
-                    feature1=row['FEATURE1'], feature2=row['FEATURE2'], feature4=row['FEATURE4'], label=row['LABEL'])
-
-    driver.close()
+    actions_dict = actions.to_dict('records')
+    session.run("""
+        UNWIND $actions AS action
+        MATCH (user:User {id: action.USERID})
+        MATCH (target:Target {id: action.TARGETID})
+        CREATE (user)-[:ACTION {id: action.ACTIONID, feature0: action.FEATURE0, feature1: action.FEATURE1,
+        feature2: action.FEATURE2, feature4: action.FEATURE4, 
+        label: action.LABEL, timestamp: action.TIMESTAMP}]->(target)
+        """, actions=actions_dict)
